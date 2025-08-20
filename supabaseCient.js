ba238bfb63aec4693ad93505e825bd0c197c3860
taku0227 <nisimura0227@gmail.com>
2025-08-21 00:47:00 +0900

    Delete supabaseCient.js

--- supabaseCient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://odklxdlsqkucrqamdpnc.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ka2x4ZGxzcWt1Y3JxYW1kcG5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MjczMTIsImV4cCI6MjA3MTIwMzMxMn0.Ya5m6WMeGmb40tZvRuGJMAWdcPJKgfDauX9JpBtB8RE";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
